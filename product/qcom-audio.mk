3f2d3d35c11d50882014f37b26afcca2f5634be2
karthick111 <karthickrko61@gmail.com>
2018-03-19 17:24:31 +0100

    karate: decommonize audio configs

---
+++ product/qcom-audio.mk
@@ -0,0 +1,4 @@
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(DEVICE_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
