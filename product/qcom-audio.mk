7b293a8a3fd4d281833fd71eabe2143a0af97c5a
karthick111 <karthickrko61@gmail.com>
2018-03-22 03:17:29 +0100

    karate: copy audio configs to vendor

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -1,4 +1,4 @@
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(DEVICE_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(DEVICE_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(DEVICE_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
