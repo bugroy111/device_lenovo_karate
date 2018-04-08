6486e7a044bffdd789adb137358e9c101319c214
FireLord1 <firelord.xda@lineageos.org>
2018-04-08 16:48:49 +0530

    karate: Make some symlinks from vendor to system
    Signed-off-by: FireLord1 <firelord.xda@lineageos.org>
    Signed-off-by: karthick111 <karthickrko61@gmail.com>

---
+++ rootdir/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+
+# Init scripts
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.device.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.device.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.symlink.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.symlink.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
