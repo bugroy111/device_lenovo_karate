ce0869f354856e2ce95c7149de6df24cfb6693fe
karthick111 <karthickrko61@gmail.com>
2018-05-01 19:02:45 +0200

    karate: Update device identifiers

--- lineage.mk
+++ lineage.mk
@@ -23,14 +23,14 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_karate
 BOARD_VENDOR := lenovo
 PRODUCT_DEVICE := karate
-PRODUCT_BRAND := lenovo
+PRODUCT_BRAND := Lenovo
 PRODUCT_MANUFACTURER := LENOVO
-
-TARGET_VENDOR_PRODUCT_NAME := karate
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="K33" \
+    PRODUCT_NAME="karate" \
     PRIVATE_BUILD_DESC="karate-user 7.0 NRD90N K33_S231_171114_ROW release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
