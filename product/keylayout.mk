0ff7f3cf09170a8b4c86811c9ddc3771c793401c
karthick111 <karthickrko61@gmail.com>
2018-06-19 01:26:36 +0530

    karate: Add device specific button jack keylayout

---
+++ product/keylayout.mk
@@ -0,0 +1,3 @@
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/keylayout/msm8937-karate-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/msm8937-karate-snd-card_Button_Jack.kl
